facec5e7786a6f27fc8c5de7ffef7dfe713f2a0a
Warittha Wangthiyong <nookzzy@gmail.com>
2020-03-11 13:44:09 +0700

    add font

--- App.js
+++ App.js
@@ -1,23 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { PacmanIndicator } from "react-native-indicators";
 import { MemoryRouter as Router, Route, Switch } from "react-router-native";
 import Home from "./src/Home";
 import Detail from './src/Detail';
+import * as Font from "expo-font";
 
-export default function App() {
-  return (
-    <Router>
-    <Switch>
-      <Route exact path="/" component={Home} />
-      <Route exact path="/detail" component={Detail} />
-    </Switch>
-  </Router>
-    // <View style={styles.container}>
-    //   <Text>Open up App.js to start working on your app!</Text>
-    // </View>
-  );
-}
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  async componentDidMount() {
+    await Font.loadAsync({
+      Roboto: require("native-base/Fonts/Roboto.ttf"),
+      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
+    });
+  }
+
+    render() {
+      return (
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/detail" component={Detail} />
+          </Switch>
+      </Router>
+      );
+    }   
+} 
+  
 
 const styles = StyleSheet.create({
   container: {
